c783836085dd69b2f1a61617c107b63d2aec1a3c
matias marino <marinomatias084@gmail.com>
2023-06-25 12:17:41 -0300

    nuevo commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 const hamburgesa = document.querySelector(".hamburgesa");
 const menu = document.querySelector(".nav-navegacion");
-/* console.log(menu);
-console.log(hamburgesa); */
+console.log(menu);
+console.log(hamburgesa);
 
 hamburgesa.addEventListener("click", () => {
   menu.classList.toggle("spread");
 });
-
 window.addEventListener("click", (e) => {
-  console.log(e.target);
+  if (
+    menu.classList.contains("spread") &&
+    e.target !== menu &&
+    e.target !== hamburgesa
+  ) {
+    menu.classList.toggle("spread");
+  }
 });
